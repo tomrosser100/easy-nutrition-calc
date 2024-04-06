ff974c8c61a1984b0e4ef5ec69c82d65b8b5aa2a
tomrosser100 <tomrosser100@gmail.com>
2024-04-06 12:35:14 +0100

    deliver to heroku

--- back/server.mjs
+++ back/server.mjs
@@ -2,13 +2,10 @@ import express from 'express'
 
 const app = express()
 
-const port = 3000
-
 app.use(express.static(process.cwd() + '/front/dist'))
+
 app.get('/**', function (req, res) {
   res.sendFile(process.cwd() + '/front/dist/index.html')
 })
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`app listening on port ${port}, http://192.168.1.35:${port}/`)
-})
+app.listen(process.env.PORT)
