6649eb493fd950d2385e5df4c4a73524e0c949e1
tomrosser100 <tomrosser100@gmail.com>
2024-04-06 12:28:27 +0100

    Update server.mjs

--- back/server.mjs
+++ back/server.mjs
@@ -1,15 +1,19 @@
 import express from 'express'
 
-
 const app = express()
 
 const port = 3000
 
 //console.log('process.cwd is', process.cwd())
 
-app.use(process.cwd(), express.static('front/dist'))
-app.get('/**', (req, res) => {
-  res.sendFile(process.cwd() + '/front/dist/index.html')
-})
+console.log('dirname:', __dirname)
+console.log('process.cwd:', process.cwd())
+console.log('full path:', path.join(__dirname, 'front/dist', 'index.html'))
+
+app.use(express.static(process.cwd() + 'front/dist'))
+app.get('/**', function (req, res) {
+  const index = path.join(__dirname, 'front/dist', 'index.html');
+  res.sendFile(index);
+});
 
 app.listen(process.env.PORT)
