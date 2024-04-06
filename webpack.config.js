68f94afbd534641b7d5c7e9881254e01ad2d2579
tomrosser100 <tomrosser100@gmail.com>
2024-04-06 11:12:21 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: "development",
-  entry: './front/src/index.jsx',
+  entry: './front/src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'front/dist'),
