d9e5b8b18a81ee9a6fdce2d256f08c43f1715906
tomrosser100 <tomrosser100@gmail.com>
2024-04-06 11:22:14 +0100

    Update server.mjs

--- back/server.mjs
+++ back/server.mjs
@@ -4,6 +4,8 @@ const app = express()
 
 const port = 3000
 
+console.log('process.cwd is', process.cwd())
+
 app.use(express.static(process.cwd() + 'front/dist'))
 app.get('/**', (req, res) => {
   res.sendFile(process.cwd() + '/front/dist/index.html')
