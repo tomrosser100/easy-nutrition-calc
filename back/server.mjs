9afa743ccdb0a6adef9fcf786b0759cb0ed96c21
tomrosser100 <tomrosser100@gmail.com>
2024-04-06 12:23:53 +0100

    server.mjs changes

--- back/server.mjs
+++ back/server.mjs
@@ -1,12 +1,13 @@
 import express from 'express'
 
+
 const app = express()
 
 const port = 3000
 
 //console.log('process.cwd is', process.cwd())
 
-app.use(express.static(process.cwd() + 'front/dist'))
+app.use(process.cwd(), express.static('front/dist'))
 app.get('/**', (req, res) => {
   res.sendFile(process.cwd() + '/front/dist/index.html')
 })
