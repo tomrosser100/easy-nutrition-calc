0c3c89ab38d6a951a679590063b30ff31b0200e0
tomrosser100 <166111661+tomrosser100@users.noreply.github.com>
2024-07-17 12:52:25 +0100

    ssl redirect added

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import sslRedirect from 'heroku-ssl-redirect'
 
 const app = express()
 
 app.use(express.static(process.cwd() + '/front/dist'))
+app.use(sslRedirect())
 
 app.get('/**', function (req, res) {
   res.sendFile(process.cwd() + '/front/dist/index.html')
