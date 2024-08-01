a35f254b1cc70dcf1f6e9ac725b52b24b257b178
tomrosser100 <tomrosser100@gmail.com>
2024-08-01 22:29:36 +0100

    merge w template

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,77 +1,76 @@
-import path from 'path'
-import nodeExternals from 'webpack-node-externals'
-import { fileURLToPath } from 'url'
-import { merge } from 'webpack-merge'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from "path";
+import nodeExternals from "webpack-node-externals";
+import { fileURLToPath } from "url";
+import { merge } from "webpack-merge";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const mode = 'production'
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const mode = "development";
 
 const frontConfig = {
-  target: "web",
-  entry: './front/src/index.tsx',
-  output: {
-    filename: 'bundle-front.js',
-    path: path.resolve(__dirname, 'front/dist'),
-    clean: true
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./front/dist",
+    hot: true,
+    compress: true,
+    port: 5006,
   },
-  plugins: [new HtmlWebpackPlugin({
-    meta: {
-      viewport:
-        "width=device-width, initial-scale=1, minimum-scale=1,shrink-to-fit=no",
-    },
-    inject: "body",
-    template: "./front/src/index.html",
-    favicon: "./front/src/favicon.png"
-  })]
-}
+  target: "web",
+  entry: "./front/src/index.tsx",
+  output: {
+    filename: "bundle-front.js",
+    path: path.resolve(__dirname, "front/dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      meta: {
+        viewport:
+          "width=device-width, initial-scale=1, minimum-scale=1,shrink-to-fit=no",
+      },
+      inject: "body",
+      template: "./front/src/index.html",
+      favicon: "./front/src/favicon.png",
+    }),
+  ],
+};
 
 const backConfig = {
   externals: [nodeExternals()],
   externalsPresets: {
-    node: true
+    node: true,
   },
-  entry: './back/src/server.ts',
+  entry: "./back/src/server.ts",
   output: {
-    filename: 'bundle-back.js',
-    path: path.resolve(__dirname, 'back/dist'),
-    clean: true
-  }
-}
+    filename: "bundle-back.js",
+    path: path.resolve(__dirname, "back/dist"),
+    clean: true,
+  },
+};
 
 const common = {
-  mode: mode,
-  devtool: 'source-map',
-  devServer: {
-    static: './front/dist'
-  },
+  mode,
   module: {
     rules: [
-      {
-        test: /\.(?:js|mjs|cjs|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        }
-      },
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.png/,
-        type: 'asset/resource'
-      }
-    ]
+        test: /\.(?:ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs', '...'],
-  }
-}
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".mjs", "..."],
+  },
+};
 
-export default [merge(common, frontConfig), merge(common, backConfig)]
+export default [merge(common, frontConfig), merge(common, backConfig)];
